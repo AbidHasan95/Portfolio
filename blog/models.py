77bb6f864cbe1a5e235dd84dee1ca10c93a2457d
AbidHasan95 <sohailhsn@gmail.com>
2019-05-07 02:50:36 +0530

    Implemented Blogs,DetailBlog and static files added

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,12 @@ class Blog(models.Model):
     body = models.TextField()
     pub_date = models.DateTimeField()
     image = models.ImageField(upload_to = 'media/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:200]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     
     path('',views.allblogs, name='allblogs.html'),
+    path('<int:blog_id>/', views.detail, name = 'detail'),
 ]
